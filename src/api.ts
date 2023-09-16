481599f6a13f8f3b3831874096c8bfd6e182cbed
Mike CHAN <76947170+OPGamePlay@users.noreply.github.com>
2023-09-16 09:42:39 +0800

    Add files via upload

---
+++ src/api.ts
@@ -0,0 +1,117 @@
+import * as config from './config';
+import * as Unit from './entity/Unit';
+import * as units from './entity/units';
+import * as Player from './entity/Player';
+import * as Projectile from './entity/Projectile';
+import * as Type from './entity/Type';
+import * as Pickup from './entity/Pickup';
+import * as rangedAction from './entity/units/actions/rangedAction';
+import * as meleeAction from './entity/units/actions/meleeAction';
+import * as Angle from './jmath/Angle';
+import * as ArrayUtil from './jmath/ArrayUtil';
+import * as Easing from './jmath/Easing';
+import * as lineSegment from './jmath/lineSegment';
+import * as math from './jmath/math';
+import * as moveWithCollision from './jmath/moveWithCollision';
+import * as Pathfinding from './jmath/Pathfinding';
+import * as Polygon2 from './jmath/Polygon2';
+import * as rand from './jmath/rand';
+import * as Rect from './jmath/Rect';
+import * as Vec from './jmath/Vec';
+
+
+import { MultiColorReplaceFilter } from '@pixi/filter-multi-color-replace';
+import * as AnimationCombos from './graphics/AnimationCombos';
+import * as FloatingText from './graphics/FloatingText';
+import * as JImage from './graphics/Image';
+import * as ImmediateModeSprites from './graphics/ImmediateModeSprites';
+import * as JpromptAll from './graphics/Jprompt';
+import * as ParticleCollection from './graphics/ParticleCollection';
+import * as Particles from './graphics/Particles';
+import * as PixiUtils from './graphics/PixiUtils';
+import * as PlanningView from './graphics/PlanningView';
+import * as CardUI from './graphics/ui/CardUI';
+import * as colors from './graphics/ui/colors';
+import * as colorUtil from './graphics/ui/colorUtil';
+import * as eventListeners from './graphics/ui/eventListeners';
+import * as keyMapping from './graphics/ui/keyMapping';
+import * as JAudio from './Audio';
+import * as VisualEffects from './VisualEffects';
+
+import * as inLiquid from './inLiquid';
+import * as LiquidPools from './LiquidPools';
+import * as modifierSummonerSickness from './modifierSummoningSickness';
+import * as Overworld from './Overworld';
+import * as Perk from './Perk';
+import * as PlayerUtils from './PlayerUtils';
+import * as JPromise from './Promise';
+import * as storage from './storage';
+import * as Subsprites from './Subsprites';
+import * as Underworld from './Underworld';
+import * as cardUtils from './cards/cardUtils';
+import * as cards from './cards/index';
+import * as cardsUtil from './cards/util';
+import * as Events from './Events';
+import { forcePush } from './cards/push';
+
+import * as commonTypes from './types/commonTypes';
+import * as particleEmitter from '@pixi/particle-emitter'
+
+
+const SpellmasonsAPI = {
+    Angle,
+    AnimationCombos,
+    ArrayUtil,
+    cards,
+    cardsUtil,
+    CardUI,
+    cardUtils,
+    colors,
+    colorUtil,
+    commonTypes, // Includes enums which are not just types
+    config,
+    Easing,
+    eventListeners,
+    Events,
+    FloatingText,
+    forcePush,
+    ImmediateModeSprites,
+    inLiquid,
+    JAudio,
+    JImage,
+    JPromise,
+    JpromptAll,
+    keyMapping,
+    lineSegment,
+    LiquidPools,
+    math,
+    meleeAction,
+    modifierSummonerSickness,
+    moveWithCollision,
+    MultiColorReplaceFilter,
+    Overworld,
+    ParticleCollection,
+    particleEmitter,
+    Particles,
+    Pathfinding,
+    Perk,
+    Pickup,
+    PixiUtils,
+    PlanningView,
+    Player,
+    PlayerUtils,
+    Polygon2,
+    Projectile,
+    rand,
+    rangedAction,
+    Rect,
+    storage,
+    Subsprites,
+    Type,
+    Underworld,
+    Unit,
+    units,
+    Vec,
+    VisualEffects,
+}
+export default SpellmasonsAPI;
